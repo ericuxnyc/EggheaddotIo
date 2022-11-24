b4c9297d90889ef0563b0f5fac00a515f6bca91f
ericuxnyc <86964628+ericuxnyc@users.noreply.github.com>
2022-11-24 16:37:55 -0500

    finished session 01 - create basic server

--- getting_started_expressjs/index.js
+++ getting_started_expressjs/index.js
@@ -3,5 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', 
-    (req, res) => res.send('Hello World! from express'));
+    (req, res) => res.send('Hello World from express'));
+
+app.get('/yo', (req, res)=> res.send("YO!"))
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
